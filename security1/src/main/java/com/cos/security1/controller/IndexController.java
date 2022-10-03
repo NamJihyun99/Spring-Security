f61855ec7af87dbad6e952f892b85d7c383aa04c
NamJihyun99 <namjihyun9936@gmail.com>
2022-10-03 17:07:48 +0900

    feat:> develop SecurityConfig

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -14,5 +15,36 @@ public class IndexController {
          * 뷰 리졸버 설정: templates(prefix), .mustache(suffix)
          * */
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 인터셉트 -> SecurityConfig 생성하면 Security 필터가 먼저 작동
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨";
     }
 }
